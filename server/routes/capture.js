61f53c3e5a560ca416463938f0ce1c14204ba779
Chad Latham <chadlatham33@gmail.com>
2016-11-09 00:35:08 -0800

    Recreate server folder and add co dependency

--- server/routes/capture.js
+++ server/routes/capture.js
@@ -35,58 +35,56 @@ const dbURL = dbURLs[environment].connection;
 const co = require('co');
 
 router.get('/capture', co.wrap(function* (req, res, next) {
-  // try {
-  //   // Connect to mongo
-  //   const db = yield mongoClient.connect(dbURL);
-  //
-  //   // Get the collection
-  //   const col = db.collection('find');
-  //
-  //   // Delete all existing documents
-  //   yield col.deleteMany();
-  //
-  //   // Insert a single document
-  //   const ret = yield col.insertMany([{ a: 1, b: 1, age: 10 }, { a: 1, c: 1, age: 20 }, { a: 1, d: 1, age: 1 }]);
-  //
-  //   assert.equal(3, ret.insertedCount);
-  //
-  //   // Manual iteration using ES6 generators !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //   // Get the cursor
-  //   // const cursor = col.find({ a: 1 }).limit(3);
-  //   //
-  //   // // Declare results array
-  //   // const docs = [];
-  //   //
-  //   // // Iterate over the cursor and fill an array
-  //   // while (yield cursor.hasNext()) {
-  //   //   docs.push(yield cursor.next());
-  //   // }
-  //
-  //   // Using toArray() to iterate the cursor !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  //   // Get first two documents that match the query
-  //   // const docs = yield col.find({ a: 1 }).limit(2).toArray();
-  //
-  //   // assert.equal(2, docs.length);
-  //
-  //   // Get all documents
-  //   // const docs = yield col.find().toArray();
-  //
-  //   // Get all docs that match the query
-  //   // const docs = yield col.find({ a: 1 }).toArray();
-  //
-  //   // Get all docs that match, skip, limit, and sorted by age
-  //   const docs = yield col.find({ a: 1 }).limit(2).skip(0).sort({ age: 1 }).toArray();
-  //
-  //   // Close the connection
-  //   db.close();
-  //
-  //   res.send(docs);
-  // }
-  // catch (err) {
-  //   next(err);
-  // }
+  try {
+    // Connect to mongo
+    const db = yield mongoClient.connect(dbURL);
 
-  res.send('success');
+    // Get the collection
+    const col = db.collection('find');
+
+    // Delete all existing documents
+    yield col.deleteMany();
+
+    // Insert a single document
+    const ret = yield col.insertMany([{ a: 1, b: 1, age: 10 }, { a: 1, c: 1, age: 20 }, { a: 1, d: 1, age: 1 }]);
+
+    assert.equal(3, ret.insertedCount);
+
+    // Manual iteration using ES6 generators !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Get the cursor
+    // const cursor = col.find({ a: 1 }).limit(3);
+    //
+    // // Declare results array
+    // const docs = [];
+    //
+    // // Iterate over the cursor and fill an array
+    // while (yield cursor.hasNext()) {
+    //   docs.push(yield cursor.next());
+    // }
+
+    // Using toArray() to iterate the cursor !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // Get first two documents that match the query
+    // const docs = yield col.find({ a: 1 }).limit(2).toArray();
+
+    // assert.equal(2, docs.length);
+
+    // Get all documents
+    // const docs = yield col.find().toArray();
+
+    // Get all docs that match the query
+    // const docs = yield col.find({ a: 1 }).toArray();
+
+    // Get all docs that match, skip, limit, and sorted by age
+    const docs = yield col.find({ a: 1 }).limit(2).skip(0).sort({ age: 1 }).toArray();
+
+    // Close the connection
+    db.close();
+
+    res.send(docs);
+  }
+  catch (err) {
+    next(err);
+  }
 }));
 
 router.post('/capture', ev(val.post), co.wrap(function* (req, res, next) {
